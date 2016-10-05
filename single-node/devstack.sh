02fac909bf3921ce0f579d70652de125c40d71dd
Eugene Kalinin <e.v.kalinin@gmail.com>
2016-10-05 18:04:38 +0300

    updated for mitaka & ubuntu 14.04

--- single-node/devstack.sh
+++ single-node/devstack.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-OPENSTACK_VERSION=stable/icehouse
-
-#-------------------------------------------------------------------------------
-
-base="`dirname \"$0\"`"
-base="`( cd \"$BASHPATH\" && pwd )`"
+OPENSTACK_VERSION=stable/mitaka
 
 sudo apt-get update -qqy
 sudo apt-get install -qqy git
@@ -20,7 +15,7 @@ if [ ! -d "$devstack_home" ]; then
   git clone https://github.com/openstack-dev/devstack.git "$devstack_home"
 fi
 
-cd "$devstack_home"
+cd "$devstack_home" && git checkout $OPENSTACK_VERSION
 
 cat > "local.conf" <<EOF
 [[local|localrc]]
@@ -43,19 +38,4 @@ FLOATING_RANGE=192.168.42.128/25
 
 EOF
 
-./unstack.sh && ./stack.sh
-
-echo "OFFLINE=True" >> local.conf
-
-# enable Internet access for the VMs
-# https://github.com/lorin/devstack-vm/issues/2#issuecomment-26503612 
-echo 1 | sudo tee /proc/sys/net/ipv4/ip_forward
-sudo iptables -t nat -I POSTROUTING -o eth0 -j MASQUERADE
-
-# openstack configuration
-source $devstack_home/openrc admin admin
-
-nova keypair-add --pub_key=~/.ssh/id_rsa.pub default	
-
-nova secgroup-add-rule default tcp 22 22 0.0.0.0/0
-nova secgroup-add-rule default icmp -1 -1 0.0.0.0/0
+./stack.sh
